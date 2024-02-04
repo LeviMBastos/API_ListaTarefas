3ea6185831cfd0d4270728a33ee10e297ebda7b9
LeviMBastos <levimatheus3@gmail.com>
2024-02-04 12:18:53 -0300

    Ajuste Swagger

--- Program.cs
+++ Program.cs
@@ -14,12 +14,17 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+app.UseStaticFiles();
+
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "API V1");
+    });
+//}
 
 app.UseHttpsRedirection();
 
